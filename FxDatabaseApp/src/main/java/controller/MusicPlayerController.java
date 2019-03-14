9f2824a550f7290189fd4429f80f80a2be724c23
Marcel Dankert <Marcel Dankert@MCL-DESKTOP-HOME>
2019-03-14 22:13:21 +0100

    *

--- FxDatabaseApp/src/main/java/controller/MusicPlayerController.java
+++ FxDatabaseApp/src/main/java/controller/MusicPlayerController.java
@@ -1,5 +1,49 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MusicPlayerController {
+
+    @FXML
+    private Button openBtn;
+
+    @FXML
+    private Button playBtn;
+
+    @FXML
+    private Button pauseBtn;
+
+    @FXML
+    private Button quitBtn;
+
+    @FXML
+    private Button closeBtn;
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+    	System.exit(0);
+    }
+
+    @FXML
+    void openDbView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pauseMusic(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playMusic(ActionEvent event) {
+
+    }
+
+    @FXML
+    void quitPlayer(ActionEvent event) {
+    	System.exit(0);
+    }
 
 }
