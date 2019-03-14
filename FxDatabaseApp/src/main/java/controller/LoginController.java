c63e8eeab76d0b8c720c09d1f77728d69b394ea1
Marcel <mindmaker8246@gmail.com>
2019-03-14 13:05:24 +0100

    first commit

---
+++ FxDatabaseApp/src/main/java/controller/LoginController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * @author s15
+ *
+ */
+public class LoginController {
+	@FXML
+	private Button closeBtn;
+	
+	public void closeWindow() {
+		System.exit(0);
+	}
+
+}
---
+++ FxDatabaseApp/src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("../FXML_Files/Login.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+		scene.getStylesheets().add(getClass().getResource("../css/application.css").toExternalForm());
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
